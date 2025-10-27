41180a4718276d435f78a89c36c7644657505846
Ahmad Alahmadi <contact@ahmad.tech>
2025-10-27 03:49:21 +0300

    theme & fonts

--- resources/js/dashboard/components/Header.vue
+++ resources/js/dashboard/components/Header.vue
@@ -1,49 +1,36 @@
 <template>
-<div class="bg-white py-2 px-2 xl:px-0 !bg-purple-700 bg-gradient-to-r from-primary-900 via-primary-800 to-indigo-700 !p-3 text-white" wire:ignore="">
-   <div class="max-w-7xl mx-auto flex items-center justify-between">
+<div class="bg-white py-2 px-2 xl:px-0 !bg-purple-700 bg-gradient-to-r from-gray-900 via-indigo-800 to-indigo-900 !p-3 text-white" wire:ignore="">
+   <div class="max-w-7xl mx-auto flex items-center justify-between gap-x-2">
       <div class="flex items-center">
          <RouterLink :to="{ name: 'home' }" class="inline-flex items-center gap-2" wire:navigate="">
             <img src="/assets/images/logo-shape-white.svg" alt="" class="size-6 !w-auto">
-            <div>
-               <span class="font-bold"></span>
-            </div>
+ 
+            <img src="/assets/images/logo-white.webp" alt="" class="size-6 !w-auto hidden md:block">
+           
          </RouterLink>
-         <RouterLink :to="{ name: 'home' }" class="inline-flex items-center gap-2" wire:navigate="">
-            <img src="/assets/images/logo-white.webp" alt="" class="size-6 !w-auto">
-            <div>
-               <span class="font-bold"></span>
-            </div>
-         </RouterLink>
-         <RouterLink :to="{ name: 'orders' }"  title="الطلبات" class="text-white flex p-1 px-2 gap-x-2 items-center justify-center shrink-0 ms-12 rounded-lg" activeClass="bg-black/40">
-            <svg class="@if-&amp;&amp; !@if-$label @endif !h-5 !w-5 inline-block size-5 ltr:-ml-1 rtl:-mr-1 dark:text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                  <path d="M9 15l6 -6"></path>
-                  <path d="M11 6l.463 -.536a5 5 0 0 1 7.071 7.072l-.534 .464"></path>
-                  <path d="M13 18l-.397 .534a5.068 5.068 0 0 1 -7.127 0a4.972 4.972 0 0 1 0 -7.071l.524 -.463"></path>
-               </svg>
+         <RouterLink :to="{ name: 'orders' }"  title="الطلبات" class="text-white flex p-1 px-2 gap-x-2 items-center justify-center shrink-0 rounded-lg ms-4 lg:ms-8" activeClass="bg-black/40">
+            <svg viewBox="0 0 24 24" class="size-5 text-white" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path opacity="0.5" d="M22 10.5V12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2H13.5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"></path> <circle cx="19" cy="5" r="3" stroke="currentColor" stroke-width="1.5"></circle> <path d="M7 14H16" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"></path> <path d="M7 17.5H13" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"></path> </g></svg>
             <span class="text-sm hidden lg:block">
-            التفاعل والطلبات
+               التفاعل والطلبات
             </span>
             <span class="text-sm block lg:hidden">
-            الطلبات
+               الطلبات
             </span>
          </RouterLink>
       </div>
+
       <div class="flex items-center gap-4">
-         <a :href="currentUser?.tenant?.storefront_url" target="_blank"  class="btn">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" class="size-[1.2em]"><path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" /></svg>
-            معاينة البروشور
-         </a>
- 
-         <a :href="currentUser?.tenant?.storefront_url"  class="focus-visible:ring-ring shadow-none focus-visible:ring-1 text-black/70 text-white !block cursor-pointer gap-2 h-9 inline-flex !bg-green-600 px-4 py-2 text-sm transition-all transition-colors bg-transparent duration-300 items-center justify-center whitespace-nowrap rounded-md hover:!bg-green-700 hover:bg-black/10 focus-visible:outline-none disabled:opacity-50 disabled:pointer-events-none lg:!hidden" wire:current="!bg-white/10" target="_blank" icon:trailing="arrow-up-left">
-            <span>
-               <svg class="!h-5 !w-5 inline-block size-5 rtl:-ml-1 ltr:-mr-1 dark:text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                  <path d="M7 7l10 10"></path>
-                  <path d="M16 7l-9 0l0 9"></path>
-               </svg>
+         <a :href="currentUser?.tenant?.storefront_url" target="_blank"  class="btn btn-primary btn-sm flex items-center gap-x-3">
+            <span class="text-sm hidden lg:flex">
+               معاينة الصفحة
             </span>
+            <span class="text-sm flex lg:hidden">
+               معاينة 
+            </span>
+            <svg viewBox="0 0 24 24" fill="none" class="size-5 -rotate-90" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M13 11L22 2M22 2H16.6562M22 2V7.34375" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path opacity="0.5" d="M22 12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"></path> </g></svg>
+
          </a>
+   
          <div class="relative" ref="dropdownRef">
             <div class="cursor-pointer flex items-center gap-x-2" @click="toggleDropdown" icon:trailing="chevron-down">
                <img src="https://ui-avatars.com/api/?background=219EBD&amp;color=fff&amp;name=Ahmad" alt="" class="object-cover rounded-full size-8 border-2 border-black/10 hover:border-black/20">
@@ -58,7 +45,7 @@
                <div class="p-0.5 bg-white rounded-b-md mt-px w-48 text-sm">
                   <div class="text-gray-500 p-2 mb-1">
                      <h1 class="font-medium text-sm [&amp;:has(+[*])]:mb-2 [*+&amp;]:mt-4 text-gray-500">{{ currentUser?.user?.name }}</h1>
-                     <div class="text-gray-500 text-sm [*+&amp;]:mt-2 dark:text-white/70">
+                     <div class="text-gray-500 text-sm [*+&amp;]:mt-2">
                         {{ currentUser?.user?.email }}
                      </div>
                   </div>
--- resources/js/dashboard/views/Account.vue
+++ resources/js/dashboard/views/Account.vue
@@ -1,9 +1,37 @@
 <template>
     <div>
         <h1>Account</h1>
+ 
+        <fieldset class="fieldset mt-4">
+            <legend class="fieldset-legend">الاسم </legend>
+            <input v-model="form.name" type="text" class="input" placeholder="الاسم" />
+        </fieldset>
+        <fieldset class="fieldset">
+            <legend class="fieldset-legend">البريد الإلكتروني</legend>
+            <input v-model="form.email" type="text" class="input" placeholder="your@email.com" dir="ltr" />
+        </fieldset>
+        <button @click="update" class="btn btn-primary">تحديث</button>
     </div>
 </template>
 
 <script setup>
+import { useAuthStore } from '../stores/auth'
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
 
+const store = useAuthStore()
+let form = ref({
+    name: '',
+    email: '',
+})
+ 
+onMounted(() => {
+    form.value.name = store.user?.name
+    form.value.email = store.user?.email || ''
+})
+ 
+const update = async () => {
+    const response = await axios.post('/api/account', form.value)
+    store.user = response.data
+}
 </script>
