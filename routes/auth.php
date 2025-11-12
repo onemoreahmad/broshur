832baee9a5d35e92727c859aef01ee63160a6d31
Ahmad Alahmadi <contact@ahmad.tech>
2025-11-13 02:05:22 +0300

    fix logout/login

--- routes/auth.php
+++ routes/auth.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
+use Illuminate\Http\Request;
   
 // auth routes 
 Route::as('auth.')
@@ -12,8 +13,16 @@ Route::as('auth.')
     Volt::route('/reset-password/{token}', 'auth.password-reset')->middleware('guest')->name('password.reset')->middleware('guest');
     Volt::route('/password/forgot-password', 'auth.forgot-password')->middleware('guest')->name('password.forgot-password')->middleware('guest');
 
-    Route::get('/logout', function () {
+    Route::get('/logout', function (Request $request) {
         auth()->logout();
+
+        $request->session()->invalidate();
+ 
+        $request->session()->regenerateToken();
+
+        // app()->instance('tenant', null);
+        config()->set('tenant', null);
+
         return redirect()->route('site.home');
     })->name('logout')->middleware('auth');
 }); 
