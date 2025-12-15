3ca32c9ab2bda28846573fe7a9e56f750ae4d6be
Ahmad Alahmadi <contact@ahmad.tech>
2025-12-15 20:55:02 +0300

    fix clients last

--- resources/views/site/components/clients.blade.php
+++ resources/views/site/components/clients.blade.php
@@ -55,7 +55,6 @@
             <button id="prevBtn" class="p-2 rounded-full hover:bg-white/10 text-neutral-400 hover:text-white transition-colors disabled:opacity-30">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-left w-5 h-5"><path d="m12 19-7-7 7-7"></path><path d="M19 12H5"></path></svg>
             </button>
-           
         </div>
         
         <!-- Mobile Swipe Indicator (Visible only on mobile) -->
